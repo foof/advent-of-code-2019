9b2d187eca32dd34997f0bb726a5a133a12ed65a
Adam <adam.n.a3@gmail.com>
2019-12-02 16:05:50 +0100

    Superior code addition

--- 1/1.js
+++ 1/1.js
@@ -2,8 +2,6 @@ const fs = require('fs')
 
 const inputRows = fs.readFileSync('./input.txt', 'utf8').split("\n")
 
-const totalFuel = inputRows
-    .map(mass => Math.floor(mass / 3) - 2)
-    .reduce((acc, fuel) => acc + fuel, 0)
+const totalFuel = inputRows.reduce((acc, fuel) => Math.floor(fuel / 3) - 2, 0)
 
 console.log(totalFuel)

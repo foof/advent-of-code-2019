39d75607ed4cff5de189b0cd80834836546a4221
Jonas Holmström <jonas.holmstrom@consector.se>
2019-12-04 08:58:21 +0100

    Slightly more readable but not really

--- 4/1.js
+++ 4/1.js
@@ -1,7 +1,8 @@
 const input = [134792, 675810]
 
 const candidates = [...Array(input[1] - input[0] + 1).keys()]
-    .map(i => i + input[0]).map(val => val.toString())
+    .map(i => i + input[0])
+    .map(val => val.toString())
     .filter(val => val.split('').sort().join('') === val && (new Set([...val.split('')])).size !== val.split('').length)
 
 console.log(candidates.length)

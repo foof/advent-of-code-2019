5fb48ee8e31ce29ceb0b7e148aeac5fb513f379c
Jonas Holmström <jonas.holmstrom@consector.se>
2019-12-04 14:43:52 +0100

    I don't even...

--- 4/2.js
+++ 4/2.js
@@ -2,6 +2,10 @@ const input = [134792, 675810]
 
 const candidates = [...Array(input[1] - input[0] + 1).keys()]
     .map(i => (i + input[0]).toString())
-    .filter(val => val.split('').sort().join('') === val && (new Set([...val.split('')])).size !== val.split('').length && [...Array(10).keys()].filter(digit => val.match(new RegExp(digit, 'g')) && val.match(new RegExp(digit, 'g')).length == 2).length)
+    .filter(val =>
+        val.split('').sort().join('') === val &&
+        (new Set([...val.split('')])).size !== val.split('').length &&
+        /(?<!1)1{2}(?!1)|(?<!2)2{2}(?!2)|(?<!3)3{2}(?!3)|(?<!4)4{2}(?!4)|(?<!5)5{2}(?!5)|(?<!6)6{2}(?!6)|(?<!7)7{2}(?!7)|(?<!8)8{2}(?!8)|(?<!9)9{2}(?!9)/.test(val)
+    )
 
 console.log(candidates.length)

3c7a2598aafe85e9299f7afb66947263e2f4ec26
Jonas Holmström <jonas.holmstrom@consector.se>
2019-12-04 09:56:23 +0100

    Merged maps

--- 4/1.js
+++ 4/1.js
@@ -1,8 +1,7 @@
 const input = [134792, 675810]
 
 const candidates = [...Array(input[1] - input[0] + 1).keys()]
-    .map(i => i + input[0])
-    .map(val => val.toString())
+    .map(i => (i + input[0]).toString())
     .filter(val => val.split('').sort().join('') === val && (new Set([...val.split('')])).size !== val.split('').length)
 
 console.log(candidates.length)
